ac367c666573b8cfb965f828abdcb39fd6278a1d
Chao <chaohsulspam@gmail.com>
2022-01-26 23:15:32 +0800

    [fix] Fix wrong path

--- update-build-number.js
+++ update-build-number.js
@@ -1,10 +1,11 @@
 const fs = require('fs');
+const PATH = 'src/data/metadata.json';
 console.log('Incrementing build number...');
-fs.readFile('src/metadata.json', function(err, content) {
+fs.readFile(PATH, function(err, content) {
     if (err) throw err;
     var metadata = JSON.parse(content);
     metadata.buildRevision = metadata.buildRevision + 1;
-    fs.writeFile('src/metadata.json', JSON.stringify(metadata), function(err){
+    fs.writeFile(PATH, JSON.stringify(metadata), function(err){
         if (err) throw err;
         console.log(`Current build number: ${metadata.buildMajor}.${metadata.buildMinor}.${metadata.buildRevision}`);
     });
